cea69dadf474ca4123226229f903784691d7cf79
Marckinglording <dev.marcoslima@gmail.com>
2026-05-13 23:28:38 -0300

    Shema SQL

--- RPG/RPG/src/DATABASE/schema.sql
+++ RPG/RPG/src/DATABASE/schema.sql
@@ -1,1 +1,14 @@
 CREATE DATABASE rpg;
+
+USE rpg;
+
+CREATE TABLE tipos_personagem (
+	id 	INT NOT NULL PRIMARY KEY AUTO_INCREMENT,
+    nome VARCHAR(40),
+    vida DOUBLE,
+    ataque DOUBLE,
+    defesa DOUBLE,
+    velocidade_movimento DOUBLE,
+    velocidade_ataque DOUBLE,
+    mana double
+);
