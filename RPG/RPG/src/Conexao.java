b63521443d7595391d612f3de5df924f8c9c7b96
Marckinglording <dev.marcoslima@gmail.com>
2026-05-13 23:40:56 -0300

    Atualização de classe de conexão

--- RPG/RPG/src/Conexao.java
+++ RPG/RPG/src/Conexao.java
@@ -8,8 +8,8 @@ public class Conexao {
         try {
 
             String url = "jdbc:mysql://localhost:3306/rpg";
-            String usuario = "root";
-            String senha = "root";
+            String usuario = "user_dev.rpg";
+            String senha = "908070";
             
             Connection conn = DriverManager.getConnection(url, usuario, senha);
 
---
+++ RPG/RPG/src/TesteDeConcexao.java
@@ -0,0 +1,7 @@
+public class TesteDeConcexao {
+    
+    public static void main(String[] args) {
+        Conexao.conectar();
+    }
+
+}
