1aac9713f55abefd2653bc7eb6b777c6693b5e5e
Marckinglording <dev.marcoslima@gmail.com>
2026-05-13 23:28:00 -0300

    Classe de conexão e primeira parte de tabela mySQL

---
+++ RPG/RPG/src/Conexao.java
@@ -0,0 +1,30 @@
+import java.sql.Connection;
+import java.sql.DriverManager;
+
+public class Conexao {
+
+    public static Connection conectar() {
+
+        try {
+
+            String url = "jdbc:mysql://localhost:3306/rpg";
+            String usuario = "root";
+            String senha = "root";
+            
+            Connection conn = DriverManager.getConnection(url, usuario, senha);
+
+            System.out.println("Conectado ao banco de dados com sucesso!");
+
+            return conn;
+
+        } catch (Exception e) {
+            
+            System.out.println("Conexão com o banco de dados falhou!\n" + e.getMessage());
+
+            return null;
+
+        }
+
+    }
+
+}
